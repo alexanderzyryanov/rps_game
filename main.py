524500aef597f1cf1d45e398ac2fc3780551cb22
Alexander <alexander.zyryanov44@gmail.com>
2018-05-09 21:15:18 +0300

    add new test entity in main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from rps_game import RPSEntityMeta
+
 from rps_game import RPSEntityRock
 
 from rps_game import RPSEntityPaper
@@ -7,8 +9,15 @@ from rps_game import RPSEntityScissors
 from rps_game import rps_game
 
 
+class RPSEntityWell(metaclass=RPSEntityMeta,
+                    great=[RPSEntityRock, RPSEntityScissors],
+                    less=[RPSEntityPaper]):
+    pass
+
+
 if __name__ == '__main__':
     print(rps_game(RPSEntityPaper, RPSEntityRock))
     print(rps_game(RPSEntityRock, RPSEntityRock))
     print(rps_game(RPSEntityScissors, RPSEntityRock))
+    print(rps_game(RPSEntityWell, RPSEntityRock))
 
